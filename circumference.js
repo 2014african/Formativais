fa02aad19606eda3e5019eab1a47cda00717a0ab
you <you@gmail.com>
2025-11-08 12:57:26 +0200

    added button, linked js to html and made consts

--- circumference.js
+++ circumference.js
@@ -1,1 +1,9 @@
 const calcButton = document.getElementById("calcButton")
+const calcButton = document.getElementById("calcButton")
+const calcButton = document.getElementById("calcButton")
+
+
+
+function circumference(){
+    console.log()
+}
